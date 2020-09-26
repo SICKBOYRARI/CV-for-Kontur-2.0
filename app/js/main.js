27efb8c408d1f617f8a8c72c222b0f4b16b4e788
SICKBOYRARI <jediz0rrr@gmail.com>
2020-09-26 15:11:27 +0700

    Adaptive added

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function(){
-
-
-
-  
+    $('#sandwichmenu').on('click', function(e) {
+        e.preventDefault();
+        $('#sandwichmenu').toggleClass("active");
+        $('.js_menu').toggleClass("active");
+        $('.header__ul').toggleClass("header__ul-active");
+        $('body').toggleClass("body-block")
+    });
 });
