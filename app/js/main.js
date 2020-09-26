d60bace020a8d4e1a574bf8f7f011ee8142888b9
SICKBOYRARI <jediz0rrr@gmail.com>
2020-09-26 18:57:18 +0700

    Added slick-slider and ontop button

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,34 @@ $(function(){
         $('.header__ul').toggleClass("header__ul-active");
         $('body').toggleClass("body-block")
     });
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/left.png'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='../img/right.png'>",
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [{
+            breakpoint: 850,
+            settings:{
+                arrows: false
+            }
+        }]
+    });
+
+    var btn = $('.ontop-button');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.removeClass('show');
+        } else {
+            btn.addClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
 });
